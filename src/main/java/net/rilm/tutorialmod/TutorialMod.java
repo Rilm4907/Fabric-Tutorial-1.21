1f2f0be9769a3b225dd452020cf51487b6224835
Rilm4907 <palikmatej@gmail.com>
2025-07-24 11:26:40 +0200

    Data Components

--- src/main/java/net/rilm/tutorialmod/TutorialMod.java
+++ src/main/java/net/rilm/tutorialmod/TutorialMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.rilm.tutorialmod.block.ModBlocks;
+import net.rilm.tutorialmod.component.ModDataComponentTypes;
 import net.rilm.tutorialmod.item.ModItemGroups;
 import net.rilm.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
@@ -19,6 +20,8 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		ModDataComponentTypes.registerDataComponents();
 
 		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 	}
--- src/main/java/net/rilm/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/net/rilm/tutorialmod/item/custom/ChiselItem.java
@@ -16,6 +16,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 import net.rilm.tutorialmod.block.ModBlocks;
+import net.rilm.tutorialmod.component.ModDataComponentTypes;
 
 import java.util.List;
 import java.util.Map;
@@ -46,6 +47,8 @@ public class ChiselItem extends Item {
                         item -> context.getPlayer().sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
 
                 world.playSound(null, context.getBlockPos(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS);
+
+                context.getStack().set(ModDataComponentTypes.COORDINATES, context.getBlockPos());
             }
 
         }
@@ -59,6 +62,10 @@ public class ChiselItem extends Item {
             tooltip.add(Text.translatable("tooltip.tutorialmod.chisel.shift_down"));
         } else {
             tooltip.add(Text.translatable("tooltip.tutorialmod.chisel"));
+        }
+
+        if (stack.get(ModDataComponentTypes.COORDINATES) != null) {
+            tooltip.add(Text.literal("Last Block Changed at " + stack.get(ModDataComponentTypes.COORDINATES)));
         }
         super.appendTooltip(stack, context, tooltip, type);
     }
